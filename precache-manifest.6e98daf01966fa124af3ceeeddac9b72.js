a9d9d3101630e5bf3129fa5a0796a6f644721946
Tilak Puli <tilakpuli15@gmail.com>
2020-07-19 11:42:43 +0530

    Updates

---
+++ precache-manifest.6e98daf01966fa124af3ceeeddac9b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12fadac3609fb8c2ffc3e1f4a353cc86",
+    "url": "/library-management-teach/index.html"
+  },
+  {
+    "revision": "c388b9f687aa29d0667e",
+    "url": "/library-management-teach/static/css/main.200df200.chunk.css"
+  },
+  {
+    "revision": "76de804b12b3e840d4e4",
+    "url": "/library-management-teach/static/js/2.35c0457e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/library-management-teach/static/js/2.35c0457e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c388b9f687aa29d0667e",
+    "url": "/library-management-teach/static/js/main.8c2c4394.chunk.js"
+  },
+  {
+    "revision": "c039515f0fb4cef5bbbf",
+    "url": "/library-management-teach/static/js/runtime-main.13994091.js"
+  }
+]);
